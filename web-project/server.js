a813ba66ef4a81b40df6ba85d3a87c71faf531c8
VimuthR <vimuth.17@cse.mrt.ac.lk>
2026-03-10 22:43:35 +1100

    Added student registration form

--- web-project/server.js
+++ web-project/server.js
@@ -7,6 +7,11 @@ const server = http.createServer((req, res) => {
 
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
+  } else if (req.url === "/register") {
+    const html = fs.readFileSync("./register.html");
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
   }
 });
 
